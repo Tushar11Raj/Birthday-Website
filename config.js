0c3794e66067d815862aa92b1cad143ee5bd55db
Tushar11Raj <52966308+Tushar11Raj@users.noreply.github.com>
2020-10-07 22:46:09 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,31 @@
+
+var config = {
+    texts: [
+        "To the ",    
+        "Greatest Woman I know",  
+        "We wish you ",
+        "more years of a sweet & happy life 😇 ",
+        
+        "🍭🍬🍭🍬 ",
+        "🎊HAPPY BIRTHDAY🎊🎊MUMMA🎊🎊 \nTushar & Ayush",
+    ],
+    /**
+    need images
+     */
+    imgs: {
+        "Greatest Woman I know":"./imgs/pic.jpeg" ,
+        "We wish you " : "./imgs/1.gif" ,
+        "more years of a sweet & happy life 😇 ": "./imgs/2.jpg",
+        "🎊HAPPY BIRTHDAY🎊🎊MUMMA🎊🎊 \nTushar & Ayush": "./imgs/pic1.jpeg" ,
+    },
+    // button description
+    desc: {
+        turn_on: "lights on 💡",
+        play: "play music 🎸",
+        bannar_coming: "decoration 🎉",
+        balloons_flying: "something missing??",
+        wish_message: "happy birthday 🎁",
+        story: "A MESSAGE FOR YOU ",
+        wishes_flying:"more wishes"
+    }
+};
